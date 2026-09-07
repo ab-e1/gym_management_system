2e235a074864958b896bb20aebe0476de95c1d3d
ab-e1 <abelghy17@gmail.com>
2026-09-08 02:27:28 +0300

    addd: added the env loading using bun built in .env loading

---
+++ src/config/env.ts
@@ -0,0 +1,4 @@
+export const port = process.env.PORT;
+export const databaseUrl = process.env.DATABASE_URL;
+export const betterAuthSecret = process.env.BETTER_AUTH_SECRET;
+export const betterAuthUrl = process.env.BETTER_AUTH_URL;
