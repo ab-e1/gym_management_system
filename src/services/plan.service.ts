eb650f1ea35a6efb88a4a1399d0b28516a2c4142
ab-e1 <abelghy17@gmail.com>
2026-09-12 00:01:01 +0300

    add: added the service layer for membership plans

---
+++ src/services/plan.service.ts
@@ -0,0 +1,66 @@
+import { connection } from "../config/connection.ts";
+import { membershipPlans } from "../db/schema.ts";
+import { eq } from "drizzle-orm";
+
+export const createPlan = async (data: {
+  name: string;
+  price: number;
+  duration: number;
+}) => {
+  const [newPlan] = await connection
+    .insert(membershipPlans)
+    .values(data)
+    .returning();
+  return { ok: true, data: newPlan, status: 201 };
+};
+
+export const getAllPlans = async () => {
+  return {
+    ok: true,
+    data: await connection.select().from(membershipPlans),
+    status: 200,
+  };
+};
+
+export const getPlanById = async (id: string) => {
+  const [plan] = await connection
+    .select()
+    .from(membershipPlans)
+    .where(eq(membershipPlans.id, id));
+
+  if (!plan) {
+    return {
+      ok: false,
+      error: {
+        code: "NOT_FOUND",
+        message: "no plan found with the given id",
+      },
+      status: 404,
+    };
+  }
+
+  return { ok: true, data: plan, status: 200 };
+};
+
+export const updatePlan = async (
+  id: string,
+  data: { name?: string; price?: number; duration?: number },
+) => {
+  const [plan] = await connection
+    .update(membershipPlans)
+    .set(data)
+    .where(eq(membershipPlans.id, id))
+    .returning();
+  if (!plan) {
+    return {
+      ok: false,
+      error: {
+        code: "NOT_FOUND",
+        message: "no plan found with the given id",
+      },
+      status: 404,
+    };
+  }
+
+  return { ok: true, data: plan, status: 200 };
+};
